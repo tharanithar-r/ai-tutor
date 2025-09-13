0cb89bcede00bc56d1971b5f4481e502defd240e
tharanithar-r <rtharanithar@gmail.com>
2025-09-14 01:50:18 +0530

    fix_socket

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:3001',
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/socket.io/:path*',
-        destination: `${process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:3001'}/socket.io/:path*`,
-      },
-    ];
+    NEXT_PUBLIC_SOCKET_URL:
+      process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3001",
   },
 };
 
